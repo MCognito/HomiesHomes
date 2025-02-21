b4b8f0cd8af055177e7cdd57adc8efd89acdaf69
Mandeep Pandya (pandyam2) <pandyam2@coventry.ac.uk>
2025-02-21 01:11:26 +0000

    Adding basic backend

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-console.log("Hello API World");
+// My Blog API
 
-function adder(a,b){
-    return a+b;
+// Set up the application and its router
+
+//const mySQL = require("mysql");
+const Koa = require("koa");
+const Router = require("koa-router");
+
+const app = new Koa();
+const router = new Router();
+
+/*
+ * Define route handler(s):
+ *
+ * This means we connect HTTP methods: GET, POST,...
+ * and the URI paths: /some/uri/path
+ * to JavaScript functions that handle the request
+ *
+ * Once defined we then add thme to the app object
+ *
+ */
+
+router.get("/homes", welcomeAPI);
+app.use(router.routes());
+
+function welcomeAPI(ctx, next) {
+  ctx.body = {
+    message: [
+      "Welcome to my Home Estate Agency API Server Test",
+      "Any Changes I Make here will be reflected onto the website with a refresh",
+    ],
+  };
 }
 
-let a = 12;
-let b = 13
+const houses = require("./backend/routes/houses");
+app.use(houses.routes());
 
-console.log(adder(a,b))
+const bodyParser = require("koa-bodyparser");
+app.use(bodyParser());
+
+// Finally, run the app as a process on a given port
+
+app.listen(3000);
